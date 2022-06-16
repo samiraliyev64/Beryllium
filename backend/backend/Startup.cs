3e400cf2e401016b5ce0e5edd8c01f661a16b570
samiraliyev64 <70ey3w0@code.edu.az>
2022-06-16 10:28:04 +0400

    feat: create admin panel

--- backend/backend/Startup.cs
+++ backend/backend/Startup.cs
@@ -46,6 +46,10 @@ namespace backend
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name:"areas",
+                    pattern:"{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
+                    );
+                endpoints.MapControllerRoute(
                     name:"default",
                     pattern:"{controller=Home}/{action=Index}/{id?}"
                     );
