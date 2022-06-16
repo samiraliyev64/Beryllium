a319c905a86f62227a1476b8f93b61ea8315f400
samiraliyev64 <70ey3w0@code.edu.az>
2022-06-16 12:34:01 +0400

    feat: CRUD done,server side done

--- backend/backend/Areas/AdminPanel/Controllers/SliderController.cs
+++ backend/backend/Areas/AdminPanel/Controllers/SliderController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using backend.DAL;
+using backend.Helpers;
+using backend.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +13,99 @@ namespace backend.Areas.AdminPanel.Controllers
     [Area("AdminPanel")]
     public class SliderController : Controller
     {
+        private AppDbContext _context { get; }
+        private IWebHostEnvironment _env { get; }
+        public SliderController(AppDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
         public IActionResult Index()
         {
+            return View(_context.Sliders);
+        }
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Slider slide)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            if (!slide.Photo.CheckFileSize(200))
+            {
+                ModelState.AddModelError("Photo","file size must be less than 200 kb");
+                return View();
+            }
+            if (!slide.Photo.CheckFileType("image/"))
+            {
+                ModelState.AddModelError("Photo", "file type must be image");
+                return View();
+            }
+            slide.Url = await slide.Photo.SaveFileAsync(_env.WebRootPath,"images");
+            await _context.Sliders.AddAsync(slide);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id==null)
+            {
+                return BadRequest();
+            }
+            var slider = _context.Sliders.Find(id);
+            if(slider == null)
+            {
+                return NotFound();
+            }
+            var path = Helper.GetPath(_env.WebRootPath,"img",slider.Url);
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+            _context.Sliders.Remove(slider);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Update(int? id)
+        {
+            if(id == null)
+            {
+                return BadRequest();
+            }
+            Slider slide = _context.Sliders.Find(id);
+            if(slide == null)
+            {
+                return NotFound();
+            }
+            return View(slide);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, Slider slide)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            Slider slideDb = _context.Sliders.Find(id);
+            if (slideDb == null)
+            {
+                return NotFound();
+            }
+            slide.Url = await slide.Photo.SaveFileAsync(_env.WebRootPath, "images");
+            var pathDb = Helper.GetPath(_env.WebRootPath,"img",slide.Url);
+            if (System.IO.File.Exists(pathDb))
+            {
+                System.IO.File.Delete(pathDb);
+            }
+            slideDb.Url = slide.Url;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ backend/backend/Helpers/Extension.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Helpers
+{
+    public static class Extension
+    {
+        public static bool CheckFileSize(this IFormFile file, int kb)
+        {
+            return file.Length / 1024 <= kb;
+        }
+        public static bool CheckFileType(this IFormFile file,string type)
+        {
+            return file.ContentType.Contains(type);
+        }
+        //public async static Task<string> SaveFileAsync(this IFormFile file,string root,params string[] folders)
+        //{
+        //    var fileName = Guid.NewGuid().ToString() + file.FileName;
+        //    var resultPath = Path.Combine(Helper.GetPath(root,folders),fileName);
+        //    using (FileStream fileStream = new FileStream(resultPath,FileMode.Create))
+        //    {
+        //        await file.CopyToAsync(fileStream);
+        //    };
+        //    return fileName;
+        //}
+        public async static Task<string> SaveFileAsync(this IFormFile file, string root, params string[] folders)
+        {
+            var fileName = Guid.NewGuid().ToString() + file.FileName;
+            var resultPath = Path.Combine(Helper.GetPath(root, folders), fileName);
+            using (FileStream filestream = new FileStream(resultPath, FileMode.Create))
+            {
+                await file.CopyToAsync(filestream);
+            };
+            return fileName;
+        }
+    }
+}
